0bd6df767f83c3d7f01c4fc79b0bdf780d3f0532
roaslin <roaslin@gmail.com>
2023-11-28 20:24:51 +0100

    Refactor Split code into main and lib

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use std::process;
+
+use minigrep::{run, Config};
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    // Valid error handling more verbose
+    // let config: Config = match config {
+    //     Ok(value) => value,
+    //     Err(err) => {
+    //         println!("Problem parsing arguments: {err}");
+    //         process::exit(1);
+    //     }
+    // };
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
