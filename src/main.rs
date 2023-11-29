8b385901e0959ac4f78e51e6feb5f93b179992be
roaslin <roaslin@gmail.com>
2023-11-29 11:33:44 +0100

    Search ignore case sensitive

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,6 @@ fn main() {
     //     }
     // };
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = run(config) {
         println!("Application error: {e}");
         process::exit(1);
