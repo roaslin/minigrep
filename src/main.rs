7e84b20fdf1f8edbfa7123df8b436ea2f6803c66
roaslin <roaslin@gmail.com>
2023-11-29 11:43:54 +0100

    Print err only on stderr

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use minigrep::{run, Config};
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     // Valid error handling more verbose
@@ -18,7 +18,7 @@ fn main() {
     // };
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
